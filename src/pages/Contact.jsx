6ce337299a57bde40a5a698827f7e9c9acc06d53
Karan Singh <thisiskaran@yusuf.local>
2026-05-11 15:39:16 +0530

    final frontend

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,11 +6,12 @@ function Contact() {
       <div className="px-6 md:px-16 py-24 text-center">
         <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
         <p className="text-gray-600">
-          Mobile: +91 96505 75884
+          Mobile:
+          {/* +91 96505 75884 */}
         </p>
         <p className="text-gray-600">
           Email:
-          <a class="mail-link" href="mailto:sarita.herbalcure@gmail.com" id="yui_3_18_1_1_1778490851460_46"> sarita.herbalcure@gmail.com</a>
+          {/* <a class="mail-link" href="mailto:sarita.herbalcure@gmail.com" id="yui_3_18_1_1_1778490851460_46"> sarita.herbalcure@gmail.com</a> */}
         </p>
       </div>
     </>
